c9cb119570dd9ff467fd78948fcba540a592e285
Christian Heinrich <christian.heinrich@digitalcareerinstitute.org>
2022-04-28 15:25:18 +0200

    frontend add user form

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,13 +1,11 @@
-import { Link } from "react-router-dom";
 import "./HomePage.css";
 
 export default function HomePage() {
   return (
     <main>
       <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo est enim
-        deserunt iste eligendi facilis et modi nam, amet officiis, aut ut
-        tenetur, quaerat ea quisquam eveniet perspiciatis similique suscipit.
+        Dieser Satz soll erklären was das hier eigentlich für ein Projekt ist,
+        was man damit machen kann und evtl. wer es gebaut hat.
       </p>
     </main>
   );
--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -1,5 +1,28 @@
 import React from "react";
+import "./UserForm.css";
 
 export default function UserForm() {
-  return <div>UserForm</div>;
+  return (
+    <main>
+      <h2>Add User</h2>
+      <form className="userform">
+        <label htmlFor="firstName">First Name:</label>
+        <input type="text" id="firstName" />
+
+        <label htmlFor="lastName">First Name:</label>
+        <input type="text" id="lastName" />
+
+        <label htmlFor="age">Age:</label>
+        <input type="number" id="age" />
+
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+          }}
+        >
+          Add
+        </button>
+      </form>
+    </main>
+  );
 }
