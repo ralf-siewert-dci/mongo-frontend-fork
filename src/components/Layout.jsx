eaeb6b57d00323948642d096410ed28739151347
Ralf Siewert (DCI) <ralf.siewert-freelancer@digitalcareerinstitute.org>
2022-05-11 11:40:26 +0200

    v2

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,7 +4,7 @@ export default function Layout() {
   return (
     <div className="layout">
       <header>
-        <h1>User-Verwaltung</h1>
+        <h1>User-Verwaltung V2</h1>
         <nav>
           <NavLink to="/">Home</NavLink>
           <NavLink to="/users">User List</NavLink>
